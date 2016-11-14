bc74c9e932ac5f455113f3c6e2d5650305ed536a
Jason Waataja <jasonswaataja@gmail.com>
2016-11-13 20:47:37 -0800

    Did some parsing

---
+++ src/com/waataja/dataanalysis/CSVData.java
@@ -0,0 +1,85 @@
+package com.waataja.dataanalysis;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
+
+public class CSVData {
+
+	private String[] columnTitles;
+	private ArrayList<String[]> rows;
+
+	public CSVData() {
+		columnTitles = null;
+		rows = new ArrayList<String[]>();
+	}
+
+	public CSVData(String filename) {
+		this();
+	}
+
+	public static String[] splitCSVLine(String line) {
+		ArrayList<String> elements = new ArrayList<String>();
+		elements.addAll(Arrays.asList(line.split(",")));
+
+		String[] asArray = new String[elements.size()];
+		for (int i = 0; i < elements.size(); i++) {
+			if (elements.get(i).startsWith("\"") && elements.get(i).endsWith("\""))
+				asArray[i] = elements.get(i).substring(1, elements.get(i).length() - 1);
+			else
+				asArray[i] = new String(elements.get(i));
+		}
+
+		return asArray;
+	}
+
+	public void loadFromFile(String filename) {
+
+		BufferedReader reader = null;
+
+		try {
+			reader = new BufferedReader(new FileReader(filename));
+
+			String line = reader.readLine();
+			
+			columnTitles = splitCSVLine(line);
+			
+			while ((line = reader.readLine()) != null)
+				rows.add(splitCSVLine(line));
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public String[] getColumnTitles() {
+		return columnTitles;
+	}
+
+	public void setColumnTitles(String[] columnTitles) {
+		this.columnTitles = columnTitles;
+	}
+
+	public ArrayList<String[]> getRows() {
+		return rows;
+	}
+
+	public void setRows(ArrayList<String[]> rows) {
+		this.rows = rows;
+	}
+}
--- src/com/waataja/dataanalysis/DataAnalysis.java
+++ src/com/waataja/dataanalysis/DataAnalysis.java
@@ -1,12 +1,35 @@
 package com.waataja.dataanalysis;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class DataAnalysis {
+	
+	public static final String DATA_FILE = "res/president_general_polls_2016.csv";
 
 	public static void main(String[] args) {
-		File myFile = new File(".");
-		System.out.println(myFile.getAbsolutePath());
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(DATA_FILE));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String line;
+		try {
+			while ((line = reader.readLine()) != null) {
+				for (String element : CSVData.splitCSVLine(line)) {
+					System.out.print(element + " ");
+				}
+				System.out.println();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
